d04a3b9facf8eda753c3963c4e5e8cd85f6c018b
ManasAyyalaraju <Manas.ayyalaraju@gmail.com>
2026-02-03 03:09:12 -0600

    Initial Commit

---
+++ Sport_Tracker-Fantasy/Sport_Tracker_FantasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sport_Tracker_FantasyApp.swift
+//  Sport_Tracker-Fantasy
+//
+//  Created by Manas Ayyalaraju on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Sport_Tracker_FantasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
